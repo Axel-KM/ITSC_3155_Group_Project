e0b1f15a32af5faf8d3886843a4523f83179971c
EC2 Default User <ec2-user@ip-172-31-36-236.us-east-2.compute.internal>
2019-10-23 04:01:49 +0000

    Set the root view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'ideas/index'
   get 'welcome/index'
+  
+  root 'ideas#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
