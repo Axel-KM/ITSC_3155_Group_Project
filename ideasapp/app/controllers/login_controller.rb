cfeb78f1997bae30e9f041966b60aacc5fc1daa3
Axel-KM <krismelemal@gmail.com>
2019-11-14 19:08:54 +0000

    added new form with radio/rank/title/text fields

--- ideasapp/app/controllers/login_controller.rb
+++ ideasapp/app/controllers/login_controller.rb
@@ -1,4 +1,8 @@
 class LoginController < ApplicationController
   def index
   end
+  
+  def create
+      render plain: params[:idea].inspect
+  end
 end
--- ideasapp/config/routes.rb
+++ ideasapp/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'login/index'
   
+  resources :ideas
+  
   root 'login#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
