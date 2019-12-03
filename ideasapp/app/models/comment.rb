72f4fc6635416eaae262289563903c1f9bc6b437
leoherman123 <homanwan611@yahoo.com>
2019-12-03 18:53:02 +0000

    Added new css for show.html

--- ideasapp/app/models/comment.rb
+++ ideasapp/app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :idea
+    belongs_to :idea
+    validates :title, presence: true, length: {minimum: 5, maximum: 30}
+    validates :text, presence: true, length: {minimum: 10}
 end
